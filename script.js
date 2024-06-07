f93f7666c4d37c4751c35f22f43e7a807843cfc9
Mohdkumailabbas <kumailabbasbbk@gmail.com>
2024-06-07 16:24:12 +0530

    weather app

---
+++ script.js
@@ -0,0 +1,11 @@
+// const apikey = "71e884598814bf14a3388b92e7eee4e2";
+// const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+
+// async function checkweather(){
+//     const response = await fetch(apiurl + `&appid=${apikey}`);
+//     var data = await response.json();
+//     console.log(data);
+// }
+
+// checkweather();
+document.querySelector(".city").innerHTML=data.name
